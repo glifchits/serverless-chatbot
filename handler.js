397e9eb95ed4ca91ba0cdf333c75fbe54033c31f
George Lifchits <george.lifchits@gmail.com>
2016-08-29 22:58:05 -0400

    added nlp_compromise to the mix, awful verb detection

--- handler.js
+++ handler.js
@@ -1,15 +1,26 @@
 'use strict';
 
+var nlp = require('nlp_compromise');
+
 // Your first function handler
 module.exports.hello = (event, context, cb) => {
   console.log('received event', event);
+  const text = event.query.text || '';
+  const terms = nlp.sentence(text).terms;
+  const verbs = terms.filter(t => t.pos.Verb);
   cb(null, {
       text: `Hello ${event.query.user_name || 'world'}!`,
       response_type: 'in_channel',
       attachments: [
         {
           text: `The date is ${new Date().toDateString()}`
-        }
+        },
+        {
+          text: `Speaker: ${event.query.user_name}`
+        },
+        {
+          text: `Verbs: ${verbs.map(v => v.text).join(', ')}`
+        },
       ],
       event,
   })
