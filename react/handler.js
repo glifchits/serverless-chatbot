cacb36e6fcb4808dfaa61371a524ec1b8edd137a
Henry Goddard <godd9170@mylaurier.ca>
2016-09-06 23:43:08 -0400

    using the id query string, query dynamodb and print the results to a react page

--- react/handler.js
+++ react/handler.js
@@ -1,27 +1,58 @@
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
 
+// DynamoDB Stuff
+var AWS = require("aws-sdk");
+AWS.config.update({region: "us-east-1"});
+var docClient = new AWS.DynamoDB.DocumentClient();
+var params = { TableName: 'users', Key : { id : null}};
 
-const Main = props => {
-  return (
-    <html>
-      <head>
-        <title>Serverless Chatbot</title>
-      </head>
-      <body>
+
+var Main = React.createClass({
+
+  renderAttributes() {
+    return Object.keys(this.props).map( attr => {
+      return(
         <div>
-          <h1>Hello {props.name || 'world'}!</h1>
-          This is from serverless
+          <span>{attr}:{this.props[attr]}</span>
         </div>
-      </body>
-    </html>
-  )
-}
+      )
+    });
+  },
+
+  render() {
+    return (
+      <html>
+        <head>
+          <title>Serverless Chatbot</title>
+        </head>
+        <body>
+          <div>
+            {this.renderAttributes()}
+            This is from serverless
+          </div>
+        </body>
+      </html>
+    )
+  }
+});
 
 
 export const main = (event, context, cb) => {
-  console.log('received event', event)
-  cb(null, ReactDOMServer.renderToStaticMarkup(
-    <Main name={event.query.user_name} />
-  ))
+  console.log('event: ', event);
+  params['Key']['id'] = event.query['id'];
+  console.log('params: ', params);
+
+  docClient.get(params, function(err, data) {
+    if (err) {
+      console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        var result = JSON.stringify(data, null, 2);
+        console.log("GetItem succeeded:", result);
+        console.log("Item:", data.Item);
+        cb(null, ReactDOMServer.renderToStaticMarkup(
+          <Main {...data.Item} />
+        ))
+    }
+  });
 }
