a04e6943720ccaec673177d0c62fab3b7ffa314e
Henry Goddard <godd9170@mylaurier.ca>
2016-09-11 23:05:48 -0400

    working status auth handler and refactored auth code

--- gcal_auth/handler.js
+++ gcal_auth/handler.js
@@ -15,9 +15,6 @@ module.exports.main = function(event, context, cb){
 			console.log('Error loading static auth HTML: ' + err);
 			return;
 		}
-		// Authorize a client with the loaded credentials, then call the
-		// Google Calendar API.
-		console.log('html: ', 'utf8', html);
 		cb(null, html.toString());
 	});
 };
